bfd496988d75aa5e759132c4e453e88be49536ac
Toni Tenchev <tonitenchev93@gmail.com>
2024-02-27 07:24:40 +0000

    slide correction on calculations

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 let btn = document.querySelector(".btn");
 let input = document.querySelector("input");
 let progressBar = document.querySelector(".progress-bar");
@@ -19,8 +17,7 @@ btn.onclick = function () {
       valueSpan.textContent = `${value}%`;
       progressBar.style.background = `conic-gradient(#4070f4 ${
         value * 3.6
-      }deg, #ededed 0deg)`;
-      // 360 / 100 = 3.6
+      }deg, #ededed 0deg)`; // 360 / 100 = 3.6
       if (value == input.value) {
         clearInterval(progress);
         input.value = "";
