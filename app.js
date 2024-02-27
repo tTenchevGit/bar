ae9bd0f04ae496b66ed8e6b0f92cceca3d120066
Toni Tenchev <tonitenchev93@gmail.com>
2024-02-27 07:22:44 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+/* Created by Tivotal */
+
+let btn = document.querySelector(".btn");
+let input = document.querySelector("input");
+let progressBar = document.querySelector(".progress-bar");
+let valueSpan = document.querySelector("span");
+
+let value = 0;
+
+btn.onclick = function () {
+  if (input.value != "" && parseInt(input.value) <= input.max) {
+    let progress = setInterval(() => {
+      if (value < input.value) {
+        value++;
+      } else {
+        value--;
+      }
+
+      valueSpan.textContent = `${value}%`;
+      progressBar.style.background = `conic-gradient(#4070f4 ${
+        value * 3.6
+      }deg, #ededed 0deg)`;
+      // 360 / 100 = 3.6
+      if (value == input.value) {
+        clearInterval(progress);
+        input.value = "";
+        input.focus();
+      }
+    }, 50);
+  }
+};
